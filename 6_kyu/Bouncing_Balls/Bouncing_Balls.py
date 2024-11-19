e01563a30c71592b4f458ac641f20322a74bec54
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-19 19:12:14 +0100

    feat(6kyu):

---
+++ 6_kyu/Bouncing_Balls/Bouncing_Balls.py
@@ -0,0 +1,9 @@
+def bouncing_ball(h, bounce, window):
+
+    return -1
+
+if __name__ == "__main__":
+        assert bouncing_ball(0.5, 1, 1) == 2
+        assert bouncing_ball(0.66, 1.5, 3) == 3
+        assert bouncing_ball(0.66, 1.5, 15) == 30 
+        assert bouncing_ball(0.75, 1.5, 21) == 30
