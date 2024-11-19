57a16c6223f79048d40f2a125184b71568193be9
MiguelRQ24 <miguelrodriguezquintas@gmail.com>
2024-11-19 19:29:35 +0100

    feat(6kyu): Añadir codigo del bouncing balls.

--- 6_kyu/Bouncing_Balls/Bouncing_Balls.py
+++ 6_kyu/Bouncing_Balls/Bouncing_Balls.py
@@ -1,9 +1,17 @@
 def bouncing_ball(h, bounce, window):
-
-    return -1
+    altura = h 
+    if h <= 0 or not 0 < bounce < 1 or window >= altura:
+          return -1
+    
+    rebotes = 1
+    altura *= bounce
+    while altura > window:
+          rebotes += 2
+          altura *= bounce
+    return rebotes
 
 if __name__ == "__main__":
-        assert bouncing_ball(0.5, 1, 1) == 2
-        assert bouncing_ball(0.66, 1.5, 3) == 3
-        assert bouncing_ball(0.66, 1.5, 15) == 30 
-        assert bouncing_ball(0.75, 1.5, 21) == 30
+        assert bouncing_ball(2, 0.5, 1) == 1
+        assert bouncing_ball(3, 0.66, 1.5) == 3
+        assert bouncing_ball(30, 0.66, 1.5) == 15
+        assert bouncing_ball(30, 0.75, 1.5) == 21
